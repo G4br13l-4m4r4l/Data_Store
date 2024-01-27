0973035936c80327a460a15d6b57336dea70ac4c
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-01-27 13:00:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StoreArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------\n\t W3lc8m3_B4ck!\n----------------------------------\n");
+
+            Console.Write("Quantos dados deseja armazenar? ");
+            int resp = Convert.ToInt32(Console.ReadLine());
+            String[] dataStore = new String[resp];
+            
+            for (int i = 0; i < dataStore.Length; i++)
+            {
+                Console.Write("Digite o dado a ser armazenado: ");
+                dataStore[i] = Convert.ToString(Console.ReadLine());
+            }
+            Console.WriteLine("\n----------------------------------\n\t DATA STORED\n----------------------------------\n");
+            foreach (String s in dataStore)
+            {
+                Show(s);
+            }
+            Console.ReadKey();
+        }
+
+        static void Show(String name) {
+            Console.WriteLine("=> Data stored: " + name);
+        }
+    }
+}
